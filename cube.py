6443d5c55d8bc0226f7c36d55976c8da78c73c14
Andres <aguirregzz97@gmail.com>
2020-10-14 21:40:26 -0500

    Create cube.py

---
+++ cube.py
@@ -0,0 +1,168 @@
+
+
+# Third Level
+int_int_operator_map = {
+    "+": "int",
+    "-": "int",
+    "*": "int",
+    "/": "float",
+    "<": "int",
+    ">": "int",
+    "<=": "int",
+    ">=": "int",
+    "==": "int",
+    "!=": "int",
+    "&": "int",
+    "|": "int",
+}
+
+int_float_operator_map = {
+    "+": "float",
+    "-": "float",
+    "*": "float",
+    "/": "float",
+    "<": "int",
+    ">": "int",
+    "<=": "int",
+    ">=": "int",
+    "==": "int",
+    "!=": "int",
+    "&": "int",
+    "|": "int",
+}
+
+int_char_operator_map = {
+    "+": "invalid",
+    "-": "invalid",
+    "*": "invalid",
+    "/": "invalid",
+    "<": "invalid",
+    ">": "invalid",
+    "<=": "invalid",
+    ">=": "invalid",
+    "==": "invalid",
+    "!=": "invalid",
+    "&": "invalid",
+    "|": "invalid",
+}
+
+float_int_operator_map = {
+    "+": "float",
+    "-": "float",
+    "*": "float",
+    "/": "float",
+    "<": "int",
+    ">": "int",
+    "<=": "int",
+    ">=": "int",
+    "==": "int",
+    "!=": "int",
+    "&": "int",
+    "|": "int",
+}
+
+float_float_operator_map = {
+    "+": "float",
+    "-": "float",
+    "*": "float",
+    "/": "float",
+    "<": "int",
+    ">": "int",
+    "<=": "int",
+    ">=": "int",
+    "==": "int",
+    "!=": "int",
+    "&": "int",
+    "|": "int",
+}
+
+float_char_operator_map = {
+    "+": "invalid",
+    "-": "invalid",
+    "*": "invalid",
+    "/": "invalid",
+    "<": "invalid",
+    ">": "invalid",
+    "<=": "invalid",
+    ">=": "invalid",
+    "==": "invalid",
+    "!=": "invalid",
+    "&": "invalid",
+    "|": "invalid",
+}
+
+char_int_operator_map = {
+    "+": "invalid",
+    "-": "invalid",
+    "*": "invalid",
+    "/": "invalid",
+    "<": "invalid",
+    ">": "invalid",
+    "<=": "invalid",
+    ">=": "invalid",
+    "==": "invalid",
+    "!=": "invalid",
+    "&": "invalid",
+    "|": "invalid",
+}
+
+char_float_operator_map = {
+    "+": "invalid",
+    "-": "invalid",
+    "*": "invalid",
+    "/": "invalid",
+    "<": "invalid",
+    ">": "invalid",
+    "<=": "invalid",
+    ">=": "invalid",
+    "==": "invalid",
+    "!=": "invalid",
+    "&": "invalid",
+    "|": "invalid",
+}
+
+char_char_operator_map = {
+    "+": "invalid",
+    "-": "invalid",
+    "*": "invalid",
+    "/": "invalid",
+    "<": "invalid",
+    ">": "invalid",
+    "<=": "invalid",
+    ">=": "invalid",
+    "==": "invalid",
+    "!=": "invalid",
+    "&": "invalid",
+    "|": "invalid",
+}
+
+# Second Level
+int_map = {
+    "int": int_int_operator_map,
+    "float": int_float_operator_map,
+    "char": int_char_operator_map,
+}
+
+float_map = {
+    "int": float_int_operator_map,
+    "float": float_float_operator_map,
+    "char": float_char_operator_map,
+}
+
+char_map = {
+    "int": char_int_operator_map,
+    "float": char_float_operator_map,
+    "char": char_char_operator_map,
+}
+
+
+# First level
+map = {
+    "int": int_map,
+    "float": float_map,
+    "char": char_map,
+}
+
+def find_return_type(type1, type2, operator):
+    return map[type1][type2][operator]
+
