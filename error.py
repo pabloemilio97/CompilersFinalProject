6e1ddf1eb668269edf04653c767f65d4dd6e92a5
Andres <aguirregzz97@gmail.com>
2020-11-23 03:57:23 -0600

    Add run time error to error

--- error.py
+++ error.py
@@ -1,10 +1,16 @@
 import sys
 
+
 def err(msg, var):
     error_output = 'ERROR: ' + msg + ' => ' + var
     print(error_output)
 
+
 def gen_err(msg):
     print(msg)
     sys.exit()
-    
+
+
+def gen_runtime_err(msg):
+    print('RUNTIME ERROR')
+    print(msg)
