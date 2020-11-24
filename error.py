5f196385c3005f5ba28b6c8b6af5d8030413a60a
Andres <aguirregzz97@gmail.com>
2020-11-24 06:00:56 -0600

    append errors to vm output

--- error.py
+++ error.py
@@ -1,18 +1,21 @@
 import sys
+import shared_vm
 
 
 def err(msg, var):
     error_output = 'ERROR: ' + msg + ' => ' + var
     print(error_output)
-
+    shared_vm.output.append(f'Compilords >> COMPILATION ERROR \n{error_output}')
+    sys.exit()
 
 def gen_err(msg):
     print(msg)
-    raise Exception
+    shared_vm.output.append(f'Compilords >> COMPILATION ERROR \n {msg}')
     sys.exit()
 
 
 def gen_runtime_err(msg):
     print('RUNTIME ERROR')
     print(msg)
+    shared_vm.output.append(f'Compilords >> RUNTIME ERROR Error \n {msg}') 
     sys.exit()
