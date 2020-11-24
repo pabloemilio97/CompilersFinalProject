ed84bb25781062e5a1749cf489c35645b12e9535
Andres <aguirregzz97@gmail.com>
2020-11-24 06:01:50 -0600

    append writes to output

--- print_file.py
+++ print_file.py
@@ -1,3 +1,6 @@
+import shared_vm
+
 def print_func(value):
     # for now this will only print in terminal, but it should add it somewhere and return request with it
     print("Wrote: ", value)
+    shared_vm.output.append(f'Compilords >> {value}')
